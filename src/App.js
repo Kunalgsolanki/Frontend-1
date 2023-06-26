84a065df6d0dd628ff191f4a3f8d57943ce95c22
advaidh-team <mithil.21beceg005@gmail.com>
2023-06-26 20:00:00 +0530

    kunal

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Signin from "./Authentication/Signin/Signin";
+
+function App() {
+  return (
+ <React.Fragment>
+
+    <Signin/>   {/* Aunthication component */}
+ </ React.Fragment>
+  );
+}
+
+export default App;
